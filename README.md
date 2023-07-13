26d3cdafed78d370ac4a8d815658217d2d8abe2f
Robert Ong <robert.ong6@ncs.com.sg>
2023-07-13 14:56:49 +0800

    upd

---
+++ README.md
@@ -0,0 +1,3 @@
+## Workshop 5
+
+export DOCKER_CERT_PATH="/root/.docker/machine/machines/docker-nginx"
--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable do_size {
 
 variable do_ssh_key {
 	type = string
-	default = "key"
+	default = "www-1"
 }
 
 variable ssh_private_key {
